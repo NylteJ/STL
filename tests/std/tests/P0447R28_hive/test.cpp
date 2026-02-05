28901101e7bfc84ff2eb98c6370fb86b8ce690dc
NylteJ <178683943+NylteJ@users.noreply.github.com>
2026-02-06 02:36:25 +0800

    5.1. Add skeleton of the normal test

---
+++ tests/std/tests/P0447R28_hive/test.cpp
@@ -0,0 +1,236 @@
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#define min(a, b) delete
+#define max(a, b) delete
+
+// TODO: These `#include`s are from the final version and not all used yet.
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <compare>
+#include <concepts>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <functional>
+#include <hive>
+#include <initializer_list>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <memory>
+#include <memory_resource>
+#include <new>
+#include <optional>
+#include <random>
+#include <ranges>
+#include <span>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#undef max
+#undef min
+
+using namespace std;
+
+namespace wrappers {
+    struct wrapper_base {};
+    template <class T>
+    concept wrapper = derived_from<T, wrapper_base>;
+
+    constexpr auto unwrap_equal_pred = [](const wrapper auto& left, const wrapper auto& right) -> bool {
+        return left.value == right.value;
+    };
+    constexpr auto unwrap_less_pred = [](const wrapper auto& left, const wrapper auto& right) -> bool {
+        return right.value > left.value; // `left.value < right.value` will confuse EDG
+    };
+} // namespace wrappers
+template <wrappers::wrapper Wrapper>
+auto unwrap(const Wrapper& wrapper) {
+    return wrapper.value;
+}
+template <semiregular T>
+    requires (!wrappers::wrapper<T>)
+T unwrap(const T& val) {
+    return val;
+}
+
+template <ranges::input_range Range>
+decltype(auto) unwrap_range(Range&& rng) {
+    using rg_value_t = ranges::range_value_t<Range>;
+    if constexpr (is_same_v<decltype(unwrap(declval<rg_value_t>())), rg_value_t>) {
+        return forward<Range>(rng);
+    } else {
+        return forward<Range>(rng) | views::transform([](const auto& val) { return unwrap(val); });
+    }
+}
+
+namespace allocators {}
+
+template <class Alloc, class Maker, class T = Alloc::value_type,
+    class EqualPred = conditional_t<wrappers::wrapper<T>, decltype(wrappers::unwrap_equal_pred), equal_to<T>>,
+    class LessPred  = conditional_t<wrappers::wrapper<T>, decltype(wrappers::unwrap_less_pred), less<T>>>
+class tests {
+private:
+    using hive_t   = hive<T, Alloc>;
+    using ptr_t    = hive_t::pointer;
+    using cptr_t   = hive_t::const_pointer;
+    using iter_t   = hive_t::iterator;
+    using riter_t  = hive_t::reverse_iterator;
+    using citer_t  = hive_t::const_iterator;
+    using criter_t = hive_t::const_reverse_iterator;
+    using diff_t   = hive_t::difference_type;
+    using size_ty  = hive_t::size_type;
+
+    using al_traits = allocator_traits<Alloc>;
+
+    static constexpr bool has_default_al = is_default_constructible_v<Alloc>;
+    static constexpr bool different_al   = !al_traits::is_always_equal::value;
+
+    static constexpr bool Cpp17MoveInsertable    = is_move_constructible_v<T>;
+    static constexpr bool Cpp17CopyInsertable    = is_copy_constructible_v<T>;
+    static constexpr bool Cpp17DefaultInsertable = is_default_constructible_v<T>;
+    static constexpr bool Cpp17CopyAssignable    = is_copy_assignable_v<T>;
+    static constexpr bool Cpp17MoveAssignable    = is_move_assignable_v<T>;
+    static constexpr bool Cpp17Swappable         = is_swappable_v<T>;
+
+    Alloc al_1;
+    Alloc al_2;
+
+    Maker raw_range_maker;
+
+    EqualPred equal_pred;
+    LessPred less_pred;
+
+    mt19937_64 rand_engine;
+
+    using raw_range_t = decltype(raw_range_maker(0, rand_engine));
+    static_assert(ranges::forward_range<raw_range_t>);
+    using raw_value_t = ranges::range_value_t<raw_range_t>;
+    static_assert(regular<raw_value_t>);
+    static_assert(three_way_comparable<raw_value_t>);
+    static_assert(is_same_v<raw_value_t, decltype(unwrap(declval<T>()))>);
+    static_assert(convertible_to<raw_value_t, T>);
+    static_assert(sizeof(raw_value_t) == sizeof(T));
+
+public:
+    tests(Alloc alloc_1, Alloc alloc_2, Maker raw_rng_maker, uint64_t random_seed = 142857,
+        EqualPred equal_pred_ = EqualPred(), LessPred less_pred_ = LessPred())
+        : al_1(alloc_1), al_2(alloc_2), raw_range_maker(raw_rng_maker), equal_pred(equal_pred_), less_pred(less_pred_),
+          rand_engine(random_seed) {
+        if constexpr (different_al) {
+            assert(al_1 != al_2);
+        }
+    }
+
+private:
+    raw_range_t gen_raw_rng(integral auto cnt) {
+        return raw_range_maker(cnt, rand_engine);
+    }
+    raw_value_t gen_raw_value() {
+        auto tmp = raw_range_maker(1, rand_engine);
+        return *ranges::begin(tmp);
+    }
+
+    static void assert_limits(const hive_t& cont, hive_limits expect) {
+        const auto limits = cont.block_capacity_limits();
+        assert(limits.min == expect.min && limits.max == expect.max);
+    }
+
+    template <ranges::input_range Expected>
+    void assert_equal(const hive_t& cont, Expected&& expect) {
+        using expected_value_t = ranges::range_value_t<Expected>;
+        if constexpr (is_same_v<expected_value_t, T>) {
+            assert(ranges::equal(cont, expect, equal_pred));
+        } else {
+            static_assert(is_same_v<expected_value_t, raw_value_t>);
+            assert(ranges::equal(unwrap_range(cont), expect));
+        }
+    }
+    template <ranges::input_range Expected>
+    void assert_permutation(const hive_t& cont, Expected&& expect) {
+        if constexpr (ranges::forward_range<Expected>) {
+            using expected_value_t = ranges::range_value_t<Expected>;
+            if constexpr (is_same_v<expected_value_t, T>) {
+                assert(ranges::is_permutation(cont, expect, equal_pred));
+            } else {
+                static_assert(is_same_v<expected_value_t, raw_value_t>);
+                assert(ranges::is_permutation(unwrap_range(cont), expect));
+            }
+        } else {
+            assert(ranges::is_permutation(
+                unwrap_range(cont), unwrap_range(forward<Expected>(expect)) | ranges::to<vector>()));
+        }
+    }
+
+public:
+    static consteval bool static_test() {
+        // TODO...
+
+        return true;
+    }
+
+    void test_all() {
+        static_assert(static_test());
+
+        // TODO...
+    }
+};
+
+template <class Raw>
+constexpr auto maker = [](integral auto cnt, uniform_random_bit_generator auto& rnd_engine) {
+    const auto single_maker = [&] {
+        if constexpr (is_integral_v<Raw>) {
+            static_assert(is_unsigned_v<Raw>);
+            return static_cast<Raw>(rnd_engine());
+        } else {
+            using value_t = ranges::range_value_t<Raw>;
+            static_assert(is_unsigned_v<value_t>);
+            static_assert(is_same_v<Raw, array<value_t, sizeof(Raw) / sizeof(value_t)>>);
+            Raw ret;
+            ranges::generate(ret, [&] { return static_cast<value_t>(rnd_engine()); });
+            return ret;
+        }
+    };
+
+    vector<Raw> ret(static_cast<size_t>(cnt));
+    ranges::generate(ret, single_maker);
+    return ret;
+};
+
+template <class T, class Oper, class... Args>
+void allocator_matrix(Oper oper, Args&&... args) {
+    using namespace allocators;
+
+    oper(tests{allocator<T>{}, allocator<T>{}, args...});
+}
+
+template <class Raw>
+void test_matrix() {
+    constexpr auto test_all    = [](auto&& test) { test.test_all(); };
+    constexpr auto static_test = []<class T>(const T&) { static_assert(T::static_test()); };
+
+    using namespace wrappers;
+
+    allocator_matrix<Raw>(test_all, maker<Raw>, 42u);
+}
+
+using trivial_medium = uint16_t; // small skipfield
+using trivial_large  = uint64_t; // big skipfield
+
+int main() {
+    test_matrix<trivial_medium>();
+    test_matrix<trivial_large>();
+
+#ifndef __EDG__
+    // sizeof(_Union) > sizeof(_Ty)
+    {
+        using value_type = array<uint8_t, 5>;
+        static_assert(sizeof(_Hive_union_provider<value_type>::_Union) > sizeof(value_type));
+        const allocator<value_type> al{};
+        tests{al, al, maker<value_type>}.test_all();
+    }
+#endif // !defined(__EDG__)
+}
